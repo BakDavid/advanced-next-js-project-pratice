2c461d8174ef72c42c9c034dba9d8ebd5e59de71
Bak David <bak.david96@gmail.com>
2022-05-11 12:59:38 +0300

    Navigating programmatically and adding basic styles with css modules

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://static.euronews.com/articles/stories/05/17/56/42/1100x619_cmsv2_dd6cf20e-a9be-50fa-beeb-a71cbd2cb9e8-5175642.jpg"
+      title="First title"
+      address="First address"
+      description="First Description"
+    />
+  );
+}
+
+export default MeetupDetails;
